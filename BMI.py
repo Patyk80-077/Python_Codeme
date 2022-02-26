c4b824ed87b2b7b41771044981ca26a41e01488f
Patyk80-077 <patykp@wp.pl>
2022-02-26 22:53:08 +0100

    Python code BMI

---
+++ BMI.py
@@ -0,0 +1,17 @@
+masa = 81
+wzrost = 1.77
+bmi = masa / (wzrost**2)
+print('Your BMI =', bmi)
+
+masa = input('podaj wage [kg]')
+masa = int(masa)
+
+wzrost = input('podaj wzrost [m]')
+wzrost = float(wzrost)
+
+bmi = masa / (wzrost**2)
+
+print('Your BMI =', bmi)
+
+
+
